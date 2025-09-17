5b6041112cf81812da4769c8996289182ec9e6c4
Dell <mahmoudalghannam1234@gmail.com>
2025-09-18 02:54:31 +0300

    add note widget

--- lib/widgets/note_widget.dart
+++ lib/widgets/note_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class NoteWidget extends StatelessWidget {
@@ -5,6 +7,52 @@ class NoteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      color: Color.fromARGB(
+        255,
+        155 + Random().nextInt(100),
+        155 + Random().nextInt(100),
+        155 + Random().nextInt(100),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "This is a title",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete, color: Colors.black, size: 32),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text(
+              "Content \n lsjdfs slkdjf s sljdfkl s\n\n\n sjdfs lsjf \n\n\n\n\n\n\n\n\n\n\n\n\n\nksjf isf shf l",
+              style: TextStyle(fontSize: 16, color: Colors.black),
+              maxLines: 2,
+            ),
+            SizedBox(height: 8),
+            Align(
+              alignment: AlignmentGeometry.bottomRight,
+              child: Text(
+                DateTime.now().toString(),
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
